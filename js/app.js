09b12ab56399b578ecf6822074be391d1ff7d387
brendanbow <56096232+brendanbow@users.noreply.github.com>
2019-11-16 13:53:27 -0500

    highlights and infofox
    added mouse events.
    there is a box to tell you what you're mousing on now.
    it gets highlighted.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //to do
 
 window.onload = function () {
-    addListeners()
+    addListeners();
 }
 
 function addListeners() {
@@ -17,37 +17,106 @@ function addListeners() {
     var breast = document.getElementById('breast');
     var colon = document.getElementById('colon');
     var testes = document.getElementById('testes');
-    
-    
-    body.addEventListener('click', function (e) {
+    var infobox = document.getElementById('infobox');
+
+
+    body.addEventListener('mouseenter', function (e) {
         //console.log("this is the body");
     });
-    skin.addEventListener('click', function (e) {
-        console.log("this is the skin");
+    skin.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the skin"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "5");
     });
-    brain.addEventListener('click', function (e) {
-        console.log("this is the brain");
+    brain.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the brain"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
     });
-    lungs.addEventListener('click', function (e) {
-        console.log("this is the lungs");
+    lungs.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the lungs"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
+
     });
-    stomach.addEventListener('click', function (e) {
-        console.log("this is the stomach");
+    stomach.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the stomach"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
+
     });
-    liver.addEventListener('click', function (e) {
-        console.log("this is the liver");
+    liver.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
+
     });
-    kidney.addEventListener('click', function (e) {
-        console.log("this is the kidney");
+    kidney.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the kidney"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
+
     });
-    breast.addEventListener('click', function (e) {
-        console.log("this is the breast");
+    breast.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the breast"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
+
     });
-    colon.addEventListener('click', function (e) {
-        console.log("this is the colon");
+    colon.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the colon"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
+
     });
-    testes.addEventListener('click', function (e) {
-        console.log("this is the testes");
+    testes.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the testes"
+        d3.select(this).attr("stroke", "gold").attr("stroke-width", "20");
+
+    });
+    infobox.addEventListener('mouseenter', function (e) {
+        infobox.innerHTML = "this is the infobox";
+    });
+
+
+    //mouse outs
+    body.addEventListener('mouseleave', function (e) {
+        //console.log("this is the body");
+    });
+    skin.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+    });
+    brain.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+    });
+    lungs.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+
+    });
+    stomach.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+
+    });
+    liver.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+
+    });
+    kidney.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+
+    });
+    breast.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "hotpink").attr("stroke-width", "15");
+
+    });
+    colon.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+
+    });
+    testes.addEventListener('mouseleave', function (e) {
+        infobox.innerHTML = "this is the infobox"
+        d3.select(this).attr("stroke", "none");
+
     });
 
 
