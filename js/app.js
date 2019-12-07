6cec767052d01cb955c94ba22b5774e458f7cade
brendanbow <56096232+brendanbow@users.noreply.github.com>
2019-12-06 23:58:38 -0500

    changed hover events to clicks (added histograms kinda)

--- js/app.js
+++ js/app.js
@@ -1,129 +1,257 @@
 //to do
 
+var topBrcaGenes, topCoadGenes, topGbmGenes, topKircGenes, topLihcGenes, topStadGenes, topPradGenes;
+var brcaData = [], coadData = [], gbmData = [], kircData = [], lihcData = [], stadData = [], pradData = [];
+
+
 window.onload = function () {
     addListeners();
+    d3.csv("data/TopGenes/BRCA_top_genes.csv", function (data) {
+        window.brca = data;
+        topBrcaGenes = Object.keys(brca[0]);
+        topBrcaGenes.shift();
+        for (i = 0; i < topBrcaGenes.length; i++) {
+            brcaData.push(topBrcaGenes[i] = {gene: topBrcaGenes[i], data: []});
+            for (j = 0; j < brca.length; j++) {
+                current = topBrcaGenes[i].gene;
+                brcaData[i].data.push(brca[j][current]);
+            }
+        }
+    });
     d3.csv("data/TopGenes/COAD_top_genes.csv", function (data) {
         window.coad = data;
+        topCoadGenes = Object.keys(coad[0]);
+        topCoadGenes.shift();
+        for (i = 0; i < topCoadGenes.length; i++) {
+            coadData.push(topCoadGenes[i] = {gene: topCoadGenes[i], data: []});
+            for (j = 0; j < coad.length; j++) {
+                current = topCoadGenes[i].gene;
+                coadData[i].data.push(coad[j][current]);
+            }
+        }
+    });
+    d3.csv("data/TopGenes/GBM_top_genes.csv", function (data) {
+        window.gbm = data;
+        topGbmGenes = Object.keys(gbm[0]);
+        topGbmGenes.shift();
+        for (i = 0; i < topGbmGenes.length; i++) {
+            gbmData.push(topGbmGenes[i] = {gene: topGbmGenes[i], data: []});
+            for (j = 0; j < gbm.length; j++) {
+                current = topGbmGenes[i].gene;
+                gbmData[i].data.push(gbm[j][current]);
+            }
+        }
+    });
+    d3.csv("data/TopGenes/KIRC_top_genes.csv", function (data) {
+        window.kirc = data;
+        topKircGenes = Object.keys(kirc[0]);
+        topKircGenes.shift();
+        for (i = 0; i < topKircGenes.length; i++) {
+            kircData.push(topKircGenes[i] = {gene: topKircGenes[i], data: []});
+            for (j = 0; j < kirc.length; j++) {
+                current = topKircGenes[i].gene;
+                kircData[i].data.push(kirc[j][current]);
+            }
+        }
+    });
+    d3.csv("data/TopGenes/LIHC_top_genes.csv", function (data) {
+        window.lihc = data;
+        topLihcGenes = Object.keys(lihc[0]);
+        topLihcGenes.shift();
+        for (i = 0; i < topLihcGenes.length; i++) {
+            lihcData.push(topLihcGenes[i] = {gene: topLihcGenes[i], data: []});
+            for (j = 0; j < lihc.length; j++) {
+                current = topLihcGenes[i].gene;
+                lihcData[i].data.push(lihc[j][current]);
+            }
+        }
+    });
+    d3.csv("data/TopGenes/STAD_top_genes.csv", function (data) {
+        window.stad = data;
+        topStadGenes = Object.keys(stad[0]);
+        topStadGenes.shift();
+        for (i = 0; i < topStadGenes.length; i++) {
+            stadData.push(topStadGenes[i] = {gene: topStadGenes[i], data: []});
+            for (j = 0; j < stad.length; j++) {
+                current = topStadGenes[i].gene;
+                stadData[i].data.push(stad[j][current]);
+            }
+        }
+    });
+    d3.csv("data/TopGenes/PRAD_top_genes.csv", function (data) {
+        window.prad = data;
+        topPradGenes = Object.keys(prad[0]);
+        topPradGenes.shift();
+        for (i = 0; i < topPradGenes.length; i++) {
+            pradData.push(topPradGenes[i] = {gene: topPradGenes[i], data: []});
+            for (j = 0; j < prad.length; j++) {
+                current = topPradGenes[i].gene;
+                pradData[i].data.push(prad[j][current]);
+            }
+        }
     });
 }
 
 function addListeners() {
 
-   
-    var body = document.getElementById('humanInner');
-    var skin = document.getElementById('skin');
-    var brain = document.getElementById('brain');
-    var lungs = document.getElementById('lungs');
-    var stomach = document.getElementById('stomach');
-    var liver = document.getElementById('liver');
-    var kidney = document.getElementById('kidney');
-    var breast = document.getElementById('breast');
-    var colon = document.getElementById('colon');
-    var prostate = document.getElementById('prostate');
-    var infobox = document.getElementById('infobox');
+
+    body = document.getElementById('humanInner');
+    skin = document.getElementById('skin');
+    brain = document.getElementById('brain');
+    lungs = document.getElementById('lungs');
+    stomach = document.getElementById('stomach');
+    liver = document.getElementById('liver');
+    kidney = document.getElementById('kidney');
+    breast = document.getElementById('breast');
+    colon = document.getElementById('colon');
+    prostate = document.getElementById('prostate');
+    infobox = document.getElementById('infobox');
 
 
-    body.addEventListener('mouseenter', function (e) {
+    body.addEventListener('click', function (e) {
         //console.log("this is the body");
     });
-    skin.addEventListener('mouseenter', function (e) {
+    skin.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the skin";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "5");
     });
-    brain.addEventListener('mouseenter', function (e) {
+    brain.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the brain";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
     });
-    lungs.addEventListener('mouseenter', function (e) {
+    lungs.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the lungs";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
 
     });
-    stomach.addEventListener('mouseenter', function (e) {
+    stomach.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the stomach";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
 
     });
-    liver.addEventListener('mouseenter', function (e) {
-        infobox.innerHTML = "this is the infobox";
+    liver.addEventListener('click', function (e) {
+        clearHighlight();
+        infobox.innerHTML = "this is the liver";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
 
     });
-    kidney.addEventListener('mouseenter', function (e) {
+    kidney.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the kidney";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
 
     });
-    breast.addEventListener('mouseenter', function (e) {
+    breast.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the breast";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
 
     });
-    colon.addEventListener('mouseenter', function (e) {
-        infobox.innerHTML = "this is the colon. <br> The top cancer-related genes are:";
-        genes = Object.keys(coad[0])
+    colon.addEventListener('click', function (e) {
+        clearHighlight();
+        infobox.innerHTML = "this is the " + "colon" + "<br> The top cancer-related genes are:";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "10");
-        for (i = 1; i < genes.length; i++) {
-            infobox.innerHTML += "<br>&emsp;&emsp;" + genes[i];
-        }
     });
-    prostate.addEventListener('mouseenter', function (e) {
+    prostate.addEventListener('click', function (e) {
+        clearHighlight();
         infobox.innerHTML = "this is the prostate";
         d3.select(this).attr("stroke", "gold").attr("stroke-width", "20");
 
     });
-    infobox.addEventListener('mouseenter', function (e) {
-        infobox.innerHTML = "this is the infobox";
-    });
+  
+}
 
+function clearHighlight(){
+    infobox.innerHTML = "";
+    d3.select(skin).attr("stroke", "none");
+    d3.select(brain).attr("stroke", "none");
+    d3.select(lungs).attr("stroke", "none");
+    d3.select(stomach).attr("stroke", "green").attr("stroke-width", "15");
+    d3.select(liver).attr("stroke", "orange").attr("stroke-width", "15");
+    d3.select(kidney).attr("stroke", "red").attr("stroke-width", "15");
+    d3.select(breast).attr("stroke", "hotpink").attr("stroke-width", "15");
+    d3.select(colon).attr("stroke", "brown").attr("stroke-width", "10");
+    d3.select(prostate).attr("stroke", "blue").attr("stroke-width", "15");
 
-    //mouse outs
-    body.addEventListener('mouseleave', function (e) {
-        //console.log("this is the body");
-    });
-    skin.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "none");
-    });
-    brain.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "none");
-    });
-    lungs.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "none");
+}
 
-    });
-    stomach.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "green").attr("stroke-width", "15");
+function drawGeneDistro() {
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 10, bottom: 10, left: 20},
+            width = 100 - margin.left - margin.right,
+            height = 200 - margin.top - margin.bottom;
 
-    });
-    liver.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "orange").attr("stroke-width", "15");
+// append the svg object to the body of the page
+    var svg = d3.select("#infobox")
+            .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .attr("class","pane")
+            .append("g")
+            .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
 
-    });
-    kidney.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "red").attr("stroke-width", "15");
+// create dummy data
+    var data = [12, 19, 11, 13, 12, 22, 13, 4, 15, 16, 18, 19, 20, 12, 11, 9]
 
-    });
-    breast.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "hotpink").attr("stroke-width", "15");
+// Compute summary statistics used for the box:
+    var data_sorted = data.sort(d3.ascending)
+    var q1 = d3.quantile(data_sorted, .25)
+    var median = d3.quantile(data_sorted, .5)
+    var q3 = d3.quantile(data_sorted, .75)
+    var interQuantileRange = q3 - q1
+    var min = q1 - 1.5 * interQuantileRange
+    var max = q1 + 1.5 * interQuantileRange
+    var axismin = q1 - 1.75 * interQuantileRange
+    var axismax = q1 + 1.75 * interQuantileRange
 
-    });
-    colon.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "brown").attr("stroke-width", "10");
+// Show the Y scale
+    var y = d3.scaleLinear()
+            .domain([axismin, axismax])
+            .range([height, 0]);
+    svg.call(d3.axisLeft(y))
 
-    });
-    prostate.addEventListener('mouseleave', function (e) {
-        infobox.innerHTML = "this is the infobox";
-        d3.select(this).attr("stroke", "blue").attr("stroke-width", "15");
+// a few features for the box
+    var center = 25
+    var width = 25
 
-    });
+// Show the main vertical line
+    svg
+            .append("line")
+            .attr("x1", center)
+            .attr("x2", center)
+            .attr("y1", y(min))
+            .attr("y2", y(max))
+            .attr("stroke", "black")
 
+// Show the box
+    svg
+            .append("rect")
+            .attr("x", center - width / 2)
+            .attr("y", y(q3))
+            .attr("height", (y(q1) - y(q3)))
+            .attr("width", width)
+            .attr("stroke", "black")
+            .style("fill", "#69b3a2")
+
+// show median, min and max horizontal lines
+    svg
+            .selectAll("toto")
+            .data([min, median, max])
+            .enter()
+            .append("line")
+            .attr("x1", center - width / 2)
+            .attr("x2", center + width / 2)
+            .attr("y1", function (d) {
+                return(y(d))
+            })
+            .attr("y2", function (d) {
+                return(y(d))
+            })
+            .attr("stroke", "black")
 
 }
